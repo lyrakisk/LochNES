f07081496d40c765ab99e09487537c026fa26535
lyrakisk <24938740+lyrakisk@users.noreply.github.com>
2024-09-13 20:15:49 +0200

    Add test case

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1070,6 +1070,7 @@ mod test_cpu {
     #[test_case("submodules/65x02/nes6502/v1/75.json")]
     #[test_case("submodules/65x02/nes6502/v1/79.json")]
     #[test_case("submodules/65x02/nes6502/v1/7d.json")]
+    #[test_case("submodules/65x02/nes6502/v1/a0.json")]
     #[test_case("submodules/65x02/nes6502/v1/a5.json")]
     #[test_case("submodules/65x02/nes6502/v1/aa.json")]
     #[test_case("submodules/65x02/nes6502/v1/a9.json")]
