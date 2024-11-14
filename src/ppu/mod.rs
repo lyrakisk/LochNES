cafa006f89902270a74f839b13385e3a4ec7c8f0
lyrakisk <24938740+lyrakisk@users.noreply.github.com>
2024-11-14 22:17:07 +0100

    Implement PPU::new()

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,10 +1,26 @@
+use std::sync::{Arc, Mutex};
+
 use crate::bus::Bus;
 
-struct PPU {
-    bus: Bus,
+// registers
+const PPUCTRL: u16 = 0x2000;
+const PPUMASK: u16 = 0x2001;
+const PPUSTATUS: u16 = 0x2002;
+const OAMADDR: u16 = 0x2003;
+const OAMDATA: u16 = 0x2004;
+const PPUSCROLL: u16 = 0x2005;
+const PPUADDR: u16 = 0x2006;
+const PPUDATA: u16 = 0x2007;
+
+pub struct PPU {
+    bus: Arc<Mutex<Bus>>,
 }
 
 impl PPU {
+    pub fn new(bus: Arc<Mutex<Bus>>) -> Self {
+        bus.lock().unwrap().mem_write(PPUSTATUS, 0b1010_0000);
+        PPU { bus: bus }
+    }
     pub fn load() {
         todo!()
     }
@@ -22,5 +38,23 @@ impl PPU {
     }
 }
 
-// test power_up_state
-// test render of single pixel
+#[cfg(test)]
+mod test_ppu {
+    use super::*;
+
+    #[test]
+    fn test_power_up_state() {
+        // Test Power-up state as documented in https://www.nesdev.org/wiki/PPU_power_up_state
+        let bus = Arc::new(Mutex::new(Bus::new()));
+        let ppu = PPU::new(bus.clone());
+
+        assert_eq!(0b0000_0000, ppu.bus.lock().unwrap().mem_read(PPUCTRL));
+        assert_eq!(0b0000_0000, ppu.bus.lock().unwrap().mem_read(PPUMASK));
+        assert_eq!(0b1010_0000, ppu.bus.lock().unwrap().mem_read(PPUSTATUS));
+        assert_eq!(0b0000_0000, ppu.bus.lock().unwrap().mem_read(PPUSCROLL));
+        assert_eq!(0b0000_0000, ppu.bus.lock().unwrap().mem_read(PPUADDR));
+        assert_eq!(0b0000_0000, ppu.bus.lock().unwrap().mem_read(PPUDATA));
+    }
+
+    // todo: test reset
+}
