5d2f40b0402142e2fe77fb410d8d9a890bed3c5d
lyrakisk <24938740+lyrakisk@users.noreply.github.com>
2024-11-27 00:06:48 +0100

    Implement set_pixel()

--- src/ppu/frame.rs
+++ src/ppu/frame.rs
@@ -1,7 +1,7 @@
 const WIDTH: usize = 256;
 const HEIGHT: usize = 240;
 // width is multiplied by 3, because each pixel needs 3 u8 values to represent its RGB colors
-const SIZE: usize = (WIDTH * 3 * HEIGHT) as usize;
+const SIZE: usize = WIDTH * 3 * HEIGHT;
 
 pub struct Frame {
     pub bytes: [u8; SIZE],
@@ -10,6 +10,15 @@ pub struct Frame {
 impl Frame {
     pub fn new() -> Self {
         Frame { bytes: [0; SIZE] }
+    }
+
+    pub fn set_pixel(&mut self, x: usize ,y: usize, rgb: (u8, u8, u8)) {
+        let base = y * 3 * WIDTH + x * 3;
+        assert!(base + 2 < self.bytes.len());
+        
+        self.bytes[base] = rgb.0;
+        self.bytes[base + 1] = rgb.1;
+        self.bytes[base + 2] = rgb.2;
     }
 }
 
