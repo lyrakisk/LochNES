af81d64ee738fc32662fe9cc086d2300cc0ad8f6
lyrakisk <24938740+lyrakisk@users.noreply.github.com>
2024-11-28 23:53:41 +0100

    Implement nametable_base() and nmi_enable()

--- src/ppu/registers/control.rs
+++ src/ppu/registers/control.rs
@@ -9,12 +9,26 @@ impl Control {
         return Control { value };
     }
 
+    pub fn nametable_base(&self) -> u16 {
+        match self.value & 0b0000_0011 {
+            0 => 0x2000,
+            1 => 0x2400,
+            2 => 0x2800,
+            3 => 0x2c00,
+            _ => panic!("Not possible")
+        }
+    }
+    
     pub fn vram_increment(&self) -> u16 {
         if (self.value & 0b0000_0100) >> 2 == 0 {
             return 1;
         } else {
             return 32;
         }
+    }
+
+    pub fn nmi_enable(&self) -> bool {
+        return (self.value & 0b1000_0000) >> 7 == 1
     }
 }
 
@@ -27,3 +41,4 @@ impl Register8 for Control {
         self.value = data;
     }
 }
+
